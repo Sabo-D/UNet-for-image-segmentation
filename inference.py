19cc44a0d99bd55f57c393b0fefb947e98d300cb
Sabo <3186280752@qq.com>
2025-03-21 23:49:52 +0800

    111

--- inference.py
+++ inference.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     inference_dataloader = dataset.inference_dataloader()
     model = model.UNet(in_channels=3, out_channels=21).to(device)
-    model.load_state_dict(torch.load('D:\AA_Pycharm_Projs\\UNet\outputs\checkpoints\\best_model_2025-03-21_13-24.pth'))
+    model.load_state_dict(torch.load('D:\AA_Pycharm_Projs\\UNet\outputs\checkpoints\\best_model_2025-03-21_23-42.pth'))
     inference.model_inference(model, inference_dataloader)
 
 
